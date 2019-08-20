88c910f561a80172edc7dcfc78f7bb6c39096655
Luis Ramirez <sdaewa22@gmail.com>
2019-08-21 00:48:07 +0100

    manipulated html/css

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ function processData(name, email, date, time) {
     var date = document.getElementById(date).value;
     var time = document.getElementById(time).value;
 
-    alert("Congratulations " + name + " your appoiment is for: " + "\n" + date + " at: " + time + "\n" + "A confirmation email has been send to:" + "\n" + email);
+    alert( name + " your appoiment is for: " + "\n" + date + " at: " + time + "\n" + "Confirmation email has been send to:" + "\n" + email);
 
 }
